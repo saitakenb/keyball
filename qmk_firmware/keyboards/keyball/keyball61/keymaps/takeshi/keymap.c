ca45692348852559fcd6fe228f8322b2302091a1
Saiki <saitakenb@gmail.com>
2026-07-09 00:03:13 +0900

    Fix missing closing brace in keymap.c

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/takeshi/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/takeshi/keymap.c
@@ -70,6 +70,7 @@ void oledkit_render_info_user(void) {
     keyball_oled_render_keyinfo();
     keyball_oled_render_ballinfo();
     keyball_oled_render_layerinfo();
+}
 #endif
 
 #ifdef COMBO_ENABLE
